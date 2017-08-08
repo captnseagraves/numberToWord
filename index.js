6491b89b2a6614ed053174d86107a1e94c46debc
Kevin Seagraves <k.s.seagraves@gmail.com>
2017-08-08 12:57:41 -0600

    added switch statements

--- index.js
+++ index.js
@@ -1,18 +1,133 @@
 
 // This function assumes I will always be given an input with two spaces after a decimal
 
-"Two thousand five hundred twenty-three and 04/100 dollars"
+// "Two thousand five hundred twenty-three and 04/100 dollars"
 
 function convert(num) {
-  let str = num.toString().replace(/[.]/g,"");
+  let arr = num.toString().split('.')
+  let hold = []
+  console.log(arr)
 
-let length = str.length
-let hold = []
+  if (arr[1]) {
+    hold.push('and ' + arr[1] + '/100 dollars')
+  }
 
-  for (let i = 0; i < str.length; i++){
+let length = arr[0].length
+
+function ones(num) {
+   switch (num) {
+      case 0:
+        return "zero"
+        break;
+      case 1:
+        return "one"
+        break;
+      case 2:
+        return "two"
+        break;
+      case 3:
+        return "three"
+        break;
+      case 4:
+        return "four"
+        break;
+      case 5:
+        return "five"
+        break;
+      case 6:
+        return "six"
+        break;
+      case 7:
+        return "seven"
+        break;
+      case 8:
+        return "eight"
+        break;
+      case 9:
+        return "nine"
+        break;
+    }
+}
+
+function teens(num) {
+   switch (num) {
+      case 10:
+        return "ten"
+        break;
+      case 11:
+        return "eleven"
+        break;
+      case 12:
+        return "twelve"
+        break;
+      case 13:
+        return "thirteen"
+        break;
+      case 14:
+        return "fourteen"
+        break;
+      case 15:
+        return "fifteen"
+        break;
+      case 16:
+        return "sixteen"
+        break;
+      case 17:
+        return "seventeen"
+        break;
+      case 18:
+        return "eighteen"
+        break;
+      case 19:
+        return "nineteen"
+        break;
+    }
+}
+
+function tens(num) {
+   switch (num) {
+      case 0:
+        return ""
+        break;
+      case 1:
+        return ""
+        break;
+      case 2:
+        return "twenty"
+        break;
+      case 3:
+        return "thirty"
+        break;
+      case 4:
+        return "fourty"
+        break;
+      case 5:
+        return "fifty"
+        break;
+      case 6:
+        return "sixty"
+        break;
+      case 7:
+        return "seventy"
+        break;
+      case 8:
+        return "eighty"
+        break;
+      case 9:
+        return "ninety"
+        break;
+    }
+}
+
+
+  for (let i = 0; i < arr[0].length; i++){
      switch (length) {
       case 1:
         hold.push('and ' + str[i-1] + str[i] + '/100 dollars');
+        length = length - 1
+        break;
+      case 2:
+        hold.push(String(str[i]));
         length--
         break;
       case 3:
@@ -31,3 +146,5 @@ let hold = []
   }
  console.log(hold)
 }
+
+convert(2523.04)
